74a96f481f8d9799af3504b2bb5363f1e1304b3e
Tasmin Hassan <150820505+Tasmin-Hassan@users.noreply.github.com>
2026-05-18 20:17:42 +0600

    update database

--- core/Database.php
+++ core/Database.php
@@ -1,26 +1,15 @@
 <?php
 
-require_once __DIR__ . "/../config/config.php";
+require_once __DIR__."/../config/config.php";
 
 class Database {
 
     public function connect(){
 
-        try {
-
-            $conn = new PDO(
-                "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
-                DB_USER,
-                DB_PASS
-            );
-
-            
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            return $conn;
-
-        } catch (PDOException $e) {
-            die("Database Connection Failed: " . $e->getMessage());
-        }
+        return new PDO(
+            "mysql:host=".DB_HOST.";dbname=".DB_NAME,
+            DB_USER,
+            DB_PASS
+        );
     }
 }
