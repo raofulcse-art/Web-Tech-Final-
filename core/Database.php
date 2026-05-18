787e1fbd73e768d31f3f5b977dc12fee2cc5fea0
Tasmin Hassan <150820505+Tasmin-Hassan@users.noreply.github.com>
2026-05-18 23:38:32 +0600

    final core

--- core/Database.php
+++ core/Database.php
@@ -1,11 +1,8 @@
 <?php
-
 require_once __DIR__."/../config/config.php";
 
 class Database {
-
     public function connect(){
-
         return new PDO(
             "mysql:host=".DB_HOST.";dbname=".DB_NAME,
             DB_USER,
