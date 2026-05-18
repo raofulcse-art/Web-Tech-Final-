0c85e79bcf3d83de5b415dede979799b200d7cf6
Tasmin Hassan <150820505+Tasmin-Hassan@users.noreply.github.com>
2026-05-18 20:13:43 +0600

    change code

--- config/config.php
+++ config/config.php
@@ -1,8 +1,5 @@
 <?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'blog_project');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
-define('BASE_URL', 'http://localhost/Web-Technologies/Project_web/public/');
+define('DB_HOST','localhost');
+define('DB_NAME','blog_project');
+define('DB_USER','root');
+define('DB_PASS','');
