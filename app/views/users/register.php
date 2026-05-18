647a5c082a664d6ace1ab2011f5a68400dec1769
Tasmin Hassan <150820505+Tasmin-Hassan@users.noreply.github.com>
2026-05-18 20:50:34 +0600

    register new

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,35 +1,46 @@
-<h2>Register Account</h2>
+<h2>Register</h2>
+<?php if(isset($_GET['error']) && $_GET['error']=='pass') 
+    echo "<p>Password must be 8+ characters</p>"; ?>
+
+<?php if(isset($_GET['error']) && $_GET['error']=='email') 
+    echo "<p>Email already exists</p>"; ?>
 
 <form method="POST">
 
-<label>Name:</label><br>
+<label>Name:</label>
+<br>
 <input type="text" name="name" required>
+
 <small>Enter your full name</small>
-<br><br>
+<br>
+<br>
 
-<label>Email:</label><br>
+<label>Email:</label>
+<br>
 <input type="email" name="email" required>
-<small>Enter valid email address</small>
-<br><br>
 
-<label>Password:</label><br>
+<small>Enter valid email address</small>
+<br>
+<br>
+
+<label>Password:</label>
+<br>
 <input type="password" name="password" required>
+
 <small>Minimum 8 characters required</small>
-<br><br>
+<br>
+<br>
 
 <label>Role:</label><br>
 <input type="radio" name="role" value="reader" checked> Reader
+
 <input type="radio" name="role" value="author"> Author
+
+<small>If author , admin approval required</small>
 <br><br>
 
 <button name="submit">Register</button>
 
-<a href="login.php">
-<button type="button">Already have account? Login</button>
-</a>
-
 </form>
 
-<?php if(isset($_GET['error'])) { ?>
-<p style="color:red;">Email already exists or error occurred</p>
-<?php } ?>
+<a href="login.php">Already have account? Login</a>
